29d774efda18a3720ea7aa48c97c8d245e8223eb
Vituu D2 <OVERroot0@gmail.com>
2026-02-07 16:49:41 +0000

    page is not running on gh pages its shwing the readme.md it shouldnt hav

--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Helper script to sync changes to GitHub
-git add .
-git commit -m "Update from Firebase Studio"
-# Try to add remote, if it exists, update the URL
-git remote add origin https://github.com/VituuD2/dj-pad.git 2>/dev/null || git remote set-url origin https://github.com/VituuD2/dj-pad.git
-git push -u origin main
