fbb1a4b7a74b5912f9cb216a31c9b94420ebbe3b
Vituu D2 <OVERroot0@gmail.com>
2026-02-21 17:24:34 +0000

    chore: new song

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,8 @@ const SOUND_MAPPING = [
   { label: 'Cepagaria', url: '/sounds/cepagaria.mp3' },
   { label: 'Ceprefere', url: '/sounds/ceprefere.mp3' },
   { label: 'Fahh', url: '/sounds/fahh.mp3' },
-  { label: 'Tailung', url: '/sounds/tailung.mp3' }
+  { label: 'Tailung', url: '/sounds/tailung.mp3' },
+  { label: 'airhorn', url: '/sounds/airhorn.mp3'}
 ];
 
 const KEY_SHORTCUTS = [
